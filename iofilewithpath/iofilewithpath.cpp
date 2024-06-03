933857e8f63dec8d82da32d0b556993614c37c6b
Pradipa Y <merakayamsstraat@gmail.com>
2024-06-03 10:09:07 +0700

    menambahhkan objek ofstream

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -7,11 +7,14 @@ int main() {
 	string baris;
 	string NamaFile;
 
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
 	ofstream outfile;
 	//membuat objek
-	outfile.open("ContohFile.txt");
+	outfile.open(NamaFile + ".txt", ios::out);
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-	//unlimited loop
-	
+}
+
