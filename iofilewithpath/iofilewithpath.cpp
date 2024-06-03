1994333e664f85224265ef65005b385e5e505bf9
Pradipa Y <merakayamsstraat@gmail.com>
2024-06-03 10:13:23 +0700

    membuat unlimiteed loop untuk menulis

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -16,5 +16,16 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+
+	//unlimited loop
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
+	outfile.close();
+
+	
 }
 
