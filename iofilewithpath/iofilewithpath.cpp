afbe0d65dabe45ef67099054d6a81c97a62f439b
Pradipa Y <merakayamsstraat@gmail.com>
2024-06-03 10:19:35 +0700

    menambahkan ifstream

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -26,6 +26,18 @@ int main() {
 	}
 	outfile.close();
 
-	
+	ifstream infile;
+	infile.open(NamaFile + ".txt", ios::out);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+
+	if (infile.is_open()) {
+		while (getline(infile, baris)) {
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
 }
 
