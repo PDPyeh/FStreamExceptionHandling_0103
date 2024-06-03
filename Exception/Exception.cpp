aee10bff993b659d67efa6ec6b917f28ab35c498
Pradipa Y <merakayamsstraat@gmail.com>
2024-06-03 10:27:33 +0700

    menambahkan catch dan cout

--- Exception/Exception.cpp
+++ Exception/Exception.cpp
@@ -10,7 +10,10 @@ int main() {
 		array<int, 3> data = { 1,2,3 };
 		cout << data.at(5) << endl;
 	}
-	
+	catch (exception& e) {
+		cout << e.what() << endl;
+	}
+	cout << "Baris program yang terakhir" << endl;
 }
 
 
